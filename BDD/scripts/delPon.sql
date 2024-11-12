896de8ec45b2e35401d0128964cf4841cf50167b
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2024-11-12 18:17:58 +0100

    ajout des pdf

---
+++ BDD/scripts/delPon.sql
@@ -0,0 +1,8 @@
+-- Suppression des tables si elles existent déjà pour éviter les conflits
+DROP TABLE IF EXISTS `Anime`;
+DROP TABLE IF EXISTS `Reserver`;
+DROP TABLE IF EXISTS `CoursRealise`;
+DROP TABLE IF EXISTS `CoursProgramme`;
+DROP TABLE IF EXISTS `Adherent`;
+DROP TABLE IF EXISTS `Poney`;
+DROP TABLE IF EXISTS `Moniteur`;
