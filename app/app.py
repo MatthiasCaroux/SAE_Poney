fa269cdfeab3c0fa9db2111fb4afaf96b594ce16
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2024-11-18 10:54:24 +0100

    ajout de la requete à la BD

--- app/app.py
+++ app/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
+import os.path
 
-
+def mkpath(p):
+    return os.path.normpath(
+        os.path.join(
+            os.path.dirname(__file__),
+            p
+        )
+    )
 
 app = Flask(__name__)
 
@@ -10,7 +18,14 @@ app.config['MYSQL_HOST'] = 'servinfo-maria'
 app.config['MYSQL_USER'] = 'niveau'
 app.config['MYSQL_PASSWORD'] = 'niveau'
 app.config['MYSQL_DB'] = 'DBniveau' #mettre sa propre BD
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'sqlite:///' + mkpath('../myapp.db')
+)
 
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 if __name__ == '__main__':
     app.run(debug=True)
