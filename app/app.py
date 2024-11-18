4872e434c78c57b94e64a3f219523af92af97e1c
Caroux Matthias <o22204986@or-iut-i002-f25.iut45.univ-orleans.fr>
2024-11-18 14:09:19 +0100

    ajout de la navbar finaloiser

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'servinfo-maria'
 app.config['MYSQL_USER'] = 'niveau'
 app.config['MYSQL_PASSWORD'] = 'niveau'
-app.config['MYSQL_DB'] = 'DBniveau' #mettre sa propre BD
+app.config['MYSQL_DB'] = 'DBniveau'
 
 
 if __name__ == '__main__':
