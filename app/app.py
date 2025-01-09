9e92f598f009c6061e14760bd6f30f8c4d35bee6
Niveau Samuel <o22204836@or-iut-i204-f02.iut45.univ-orleans.fr>
2025-01-09 14:42:35 +0100

    modification minuer

--- app/app.py
+++ app/app.py
@@ -6,17 +6,17 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 
-# app.config['MYSQL_HOST'] = 'servinfo-maria'
-# app.config['MYSQL_USER'] = 'niveau'
-# app.config['MYSQL_PASSWORD'] = 'niveau'
-# app.config['MYSQL_DB'] = 'DBniveau' #mettre sa propre BD
-# app.config['SECRET_KEY'] = 'secret'
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'matthias1'
-app.config['MYSQL_DB'] = 'sae_poney'
+app.config['MYSQL_HOST'] = 'servinfo-maria'
+app.config['MYSQL_USER'] = 'niveau'
+app.config['MYSQL_PASSWORD'] = 'niveau'
+app.config['MYSQL_DB'] = 'DBniveau' #mettre sa propre BD
 app.config['SECRET_KEY'] = 'secret'
+
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'matthias1'
+# app.config['MYSQL_DB'] = 'sae_poney'
+# app.config['SECRET_KEY'] = 'secret'
 
 
 
